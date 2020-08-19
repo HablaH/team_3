435b7638f2a3b7dca1b7df1c8410131a0f0649ee
Harald S <harald@getacademy.no>
2020-08-19 14:22:15 +0200

    teamoppgave

--- rutefunk.js
+++ rutefunk.js
@@ -1,9 +1,36 @@
-// Rutenett størelse
-var sizeX = 4;
-var sizeY = 4;
+var diskoEnabled = false;
+var colors = ['Chartreuse', 'BlueViolet', 'Crimson', 'DarkOliveGreen', 'DarkGoldenRod', 'DarkSalmon', 'FireBrick', 'GreenYellow', 'HotPink', 'LemonChiffon', 'MediumSpringGreen', 'MediumTurquoise', 'MediumVioletRed', 'PowderBlue', 'PaleVioletRed','Tomato']
 
-function Test() {
+setInterval(diskoMode, 200);
 
+function RandomColor(element)
+{
+	element.style.backgroundColor = colors[Math.round(Math.random() * colors.length)];
+}
+
+
+function diskoToggle()
+{
+	if (diskoEnabled == true)
+	{
+		diskoEnabled = false
+	}
+	else
+	{
+		diskoEnabled = true
+		document.getElementById('media').style.display = 'initial';
+    }
+}
+
+function diskoMode()
+{
+	if (diskoEnabled)
+	{
+		for (var i = 0; i < 9; i++)
+		{
+			RandomColor(document.getElementById(i));
+		}	
+	}
 }
 
 
