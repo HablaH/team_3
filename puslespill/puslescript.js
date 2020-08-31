99d60b905621dbdbc6930130f7c2feaa4e37da21
Harald S <harald@getacademy.no>
2020-08-31 17:07:15 +0200

    funker pusle

--- puslespill/puslescript.js
+++ puslespill/puslescript.js
@@ -1,14 +1,66 @@
-function testDrag(event)
-{
-	event.dataTransfer.setData("Image", event.target.id);
+
+var dragElement = null; // start drag element
+var endElement = null;	// end drag element
+
+function MouseOver(element) {
+	if (element == dragElement) {
+		return;
+	}
+
+	endElement = element;
+	//console.log("End: " + element.style.backgroundImage);
 }
-function allowDrop(event)
-{
-	event.preventDefault();
+
+function DragStart(element) {
+
+	// If draggable false
+	if (element.style.backgroundImage == 'url("tom.png")')
+	{
+		return;
+	}
+
+	dragElement = element;
+	//console.log("Start: " + element.style.backgroundImage);
 }
-function testDrop(event)
+
+function DragEnd() {
+	//console.log('Set elements');
+
+	//console.log(String(dragElement.style.backgroundImage));
+	//console.log('Text:' + endElement.innerText);
+
+	if (!endElement.draggable)
+	{
+		dragElement = null;
+		endElement = null;
+
+		return;
+	}
+
+	// If correct position
+	if (dragElement.style.backgroundImage == endElement.innerText)
+	{
+		endElement.style.border = 'none';
+		endElement.draggable = false;
+		endElement.style.userSelect = 'none';
+
+	}
+
+	if (dragElement != null && endElement != null) {
+		var dragColor = dragElement.style.backgroundImage;
+		var endColor = endElement.style.backgroundImage;
+
+		dragElement.style.backgroundImage = endColor;
+		endElement.style.backgroundImage = dragColor;
+	}
+
+	dragElement = null;
+	endElement = null;
+}
+
+function OnDragEnd(element)
 {
-	event.preventDefault();
-	var data = event.dataTransfer.getData("Image");
-	event.target.appendChild(document.getElementById(data));
+	if (endElement == element) {
+		endElement = null;
+	}
 }
