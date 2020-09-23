a36283f8e2513067fc194ca25b49f78b01d9d1b0
Harald S <harald@getacademy.no>
2020-09-23 15:34:40 +0200

    bilder + css + hp bars og bedre func

--- fight/lib.js
+++ fight/lib.js
@@ -1,9 +1,12 @@
 //model
 let playerHealth = 100;
-let npcHealth = 100;
+const playerHealthMax = 100;
+let npcHealth = 250;
+const npcHealthMax = 250;
 let end;
-const npcBilder = [];
-const playerBilder = [];
+const npcBilder = ["url('bilder/Terje2.png')", "url('bilder/TerjeLowHP.png')"];
+const playerBilder = ["url('bilder/AylaFullHP.png')", "url('bilder/AylaLowHP.png')", "url('bilder/AylaNoHP.png')" ];
+const bilde = document.documentElement.style;
 
 //view
 show();
@@ -14,35 +17,38 @@ function show()
     document.getElementById('app').innerHTML = 
         ` 
         <div class="main">
-            <div class="playerNavn">PLAYER</div>
-            <div class="playerBilde">playerBilde</div>
-            <div class="npcNavn">npcNavn</div>
-            <div class="npcBilde">npcBilde</div>
-            <div class="playerHp">player: ${playerHealth}</div>
-            <div class="npcHp">npc: ${npcHealth}</div>
+            <div class="playerNavn">Ayla Dragonborn</div>
+            <div class="playerBilde"></div>
+            <div class="npcNavn">Terjegur Grakk</div>
+            <div class="npcBilde"></div>
+            <div class="playerHp"><div class="hpBar" style="width: ${playerHealth}%; background-color: #4CAF50;">${playerHealth}</div></div>
+            <div class="npcHp"><div class="hpBar" style="width: ${prosenter(npcHealth, npcHealthMax)}%; background-color: #4CAF50;">${npcHealth}</div></div>
             <div class="VS">${winner}</div>
             <div class="attack"><button onclick="attack()" ${ended}>ATTACK</button></div>
         </div>
         `;
 }
-
+//player:<progress id="healthPlayer" value="${playerHealth}" max="100"></progress>${playerHealth}
 //controller
 
 
 function checkHealthNpc() {
 
-    if (npcHealth > 50) {
-        //bytt bilde 1 
+    if (prosenter(npcHealth, npcHealthMax) > 50) {
+        //bytt bilde 1 npcBilder[0]
         console.log('masse liv');
+        bilde.setProperty('--npcbilde', npcBilder[0]);
     }
-    else if (npcHealth <= 50 && npcHealth >= 1) {
-        //bytt bilde 2
+    else if (prosenter(npcHealth, npcHealthMax) <= 50 && npcHealth >= 1) {
+        //bytt bilde 2 npcBilder[1]
         console.log('snart ded');
+        bilde.setProperty('--npcbilde', npcBilder[1]);
     }
-    else if (npcHealth <= 0) {
+    else if (prosenter(npcHealth, npcHealthMax) <= 0) {
         end = 'yey';
-        //bytt bilde 3
+        //bytt bilde 3 npcBilder[2]
         console.log('yey');
+        bilde.setProperty('--npcbilde', npcBilder[2]);
     }
 
 
@@ -52,11 +58,13 @@ function checkHealthPlayer() {
     if (playerHealth > 50) {
         //bytt bilde 1 
         console.log('masse liv');
+        bilde.setProperty('--playerbilde', playerBilder[0]);
     }
     else if (playerHealth <= 50 && playerHealth >= 1)
     {
         //bytt bilde 2
         console.log('snart ded');
+        bilde.setProperty('--playerbilde', playerBilder[1]);
     }
     else if (playerHealth <= 0)
     {
@@ -64,6 +72,7 @@ function checkHealthPlayer() {
         //bytt bilde 3
         console.log('ded');
         end = 'taper';
+        bilde.setProperty('--playerbilde', playerBilder[2]);
     }
     
    
@@ -72,11 +81,13 @@ function checkHealthPlayer() {
 function attack()
 {
     
-        npcHealth = npcHealth - playerAttack();
+    npcHealth = npcHealth - playerAttack();
+    npcHealth <= 0 ? npcHealth = 0 : npcHealth;
         checkHealthNpc();
         show();
     if (end != 'yey') {
         playerHealth = playerHealth - npcAttack();
+        playerHealth <= 0 ? playerHealth = 0 : playerHealth;
         checkHealthPlayer();
         show();
     } else { return; }
@@ -88,7 +99,7 @@ function playerAttack()
 {
     const playerDmg = Math.floor(Math.random() * 10) + 1; 
     const critChance = Math.floor(Math.random() * 100);
-    if (critChance <= 20 && critChance >= 0)
+    if (critChance <= 50 && critChance >= 0)
     {
         console.log('crit ' + (playerDmg * 2))
         return (playerDmg * 2);
@@ -99,9 +110,9 @@ function playerAttack()
 
 function npcAttack()
 {
-    const npcDmg = Math.floor(Math.random() * 20) + 1;
+    const npcDmg = Math.floor(Math.random() * 15) + 1;
     const missChance = Math.floor(Math.random() * 100);
-    if (missChance <= 20 && missChance >= 0) {
+    if (missChance <= 50 && missChance >= 0) {
         console.log('miss')
         return (npcDmg * 0);
     }
@@ -115,4 +126,8 @@ function winCheck()
     } else if (end == 'yey') {
         return 'Du vant!';
     } else { return 'VS'; }
+}
+//hjelpefunksjon %
+function prosenter(val, max) {
+    return ((val * 100) / max);
 }
